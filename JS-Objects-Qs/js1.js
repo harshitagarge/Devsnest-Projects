ea14169bdfdd6eeed4dbbf29222d28e6124a19dc
horcrux <67372760+horcruxcodes@users.noreply.github.com>
2021-06-24 04:19:08 +0530

    Update js1.js

--- JS-Objects-Qs/js1.js
+++ JS-Objects-Qs/js1.js
@@ -1,1 +1,11 @@
+// 1. Write a JavaScript program to list the properties of a JavaScript object.
 
+l_prop = (input) => {
+  arr = [];
+  for (i in input) arr.push(i);
+  return arr.join(", ");
+};
+
+var student = { name: "David Rayy", sclass: "VI", rollno: 12 };
+
+console.log(l_prop(student));
